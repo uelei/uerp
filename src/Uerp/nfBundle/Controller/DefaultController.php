5011590b25cd4d4dfb5dee66d4fdf451c941ecfb
uelei <github@uelei.com>
2015-06-02 17:01:14 -0300

    clean the mess

--- src/Uerp/nfBundle/Controller/DefaultController.php
+++ src/Uerp/nfBundle/Controller/DefaultController.php
@@ -8,14 +8,10 @@ use DanfeNFePHP;
 
 class DefaultController extends Controller
 {
-    /**
-     *
-     */
     public function indexAction()
     {
-//        require_once('../../libs/NFe/DanfeNFePHP.class.php');
 
-//$arq = $_GET['nfe'];
+        //$arq = $_GET['nfe'];
         $arq = '..//vendor/nfephp-org/nfephp/exemplos/xml/35101158716523000119550010000000011003000000-nfe.xml';
 
         if (is_file($arq)) {
@@ -24,9 +20,6 @@ class DefaultController extends Controller
             $id = $danfe->montaDANFE();
             $teste = $danfe->printDANFE($id.'.pdf', 'I');
         }
-
-
-
 
     }
 }
